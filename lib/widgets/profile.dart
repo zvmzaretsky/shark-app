bb8ef36407e4f1dae42d3b570f978e1675ea141f
Umair Jibran <umairjibran7@gmail.com>
2020-12-06 16:53:26 +0500

    set user name and email display

--- lib/widgets/profile.dart
+++ lib/widgets/profile.dart
@@ -5,22 +5,27 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    String _name = "name";
-    String _email = "email";
+    String _name = "JohnDoe";
+    String _email = "johndoe@company.com";
 
     return Container(
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
       child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                   _name,
-                  style: Theme.of(context).textTheme.headline1,
+                  style: Theme.of(context).textTheme.headline1.copyWith(
+                    fontSize: 30,
+                  )
               ),
               Text(
                 _email,
-                style: Theme.of(context).textTheme.headline1,
+                style: Theme.of(context).textTheme.headline1.copyWith(
+                    fontSize: 30,
+                  ),
               ),
             ],
           )
