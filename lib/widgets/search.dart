96f1c148abc35a6f3be790aecc7ef5d58caacf3e
Umair Jibran <umairjibran7@gmail.com>
2020-12-06 13:31:49 +0500

    add home screen

---
+++ lib/widgets/search.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Center(child: Text("Search"))
+    );
+  }
+}
