828a9559551ccf8853b14caafcb52b957b529fac
zvmzaretsky <70215062+zvmzaretsky@users.noreply.github.com>
2020-12-06 13:02:32 +0200

    profile edited, search_card.dart added

--- lib/widgets/profile.dart
+++ lib/widgets/profile.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+
+    String _name = "name";
+    String _email = "email";
+
     return Container(
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
-      child: Center(child: Text("Profile"))
+      child: Center(
+          child: Column(
+            children: [
+              Text(
+                  _name,
+                  style: Theme.of(context).textTheme.headline1,
+              ),
+              Text(
+                _email,
+                style: Theme.of(context).textTheme.headline1,
+              ),
+            ],
+          )
+      )
     );
   }
 }
